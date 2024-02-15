ac2926983dbdff6e04c8cce6a7f241b3d10dbe50
Abdu-alyousef <boud.yousef95@gmail.com>
2024-02-15 18:15:40 +0100

    Added custom Button and Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-
+    <>
+    <Modal actionLabel="Submit" isOpen title="hello"/>
       <Layout>
       <Component {...pageProps} />
       </Layout>
-  
+      </>
  
   );
 }
