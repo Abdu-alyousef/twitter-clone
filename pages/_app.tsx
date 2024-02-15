cb12d08cc319ee062a9427473dfe65717a23e948
Abdu-alyousef <boud.yousef95@gmail.com>
2024-02-15 20:00:45 +0100

    Added Form input

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
-
 import Layout from "@/components/Layout";
-import Modal from "@/components/Modal";
+import LoginModal from "@/components/modals/LoginModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Modal actionLabel="Submit" isOpen title="hello"/>
-      <Layout>
+    <LoginModal />
+    <Layout>
       <Component {...pageProps} />
-      </Layout>
-      </>
- 
+    </Layout>
+    </>
   );
 }
